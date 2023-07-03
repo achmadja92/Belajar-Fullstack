f958cb456ac9a6a21c4fb76e180e8216d6b8a76d
achmad jamal saputra <jamalsaputra.js@gmail.com>
2023-07-03 12:57:30 +0700

    tes

---
+++ React/inventory-masjid/src/Components/Navbar.jsx
@@ -0,0 +1,26 @@
+// class component
+
+//ketik rcc
+// import React, { Component } from "react";
+
+// export default class Navbar extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <h3>Navbar</h3>
+//       </div>
+//     );
+//   }
+// }
+
+//rfc
+// function component //lebih sedikit menggunakan syntax
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div>
+      <h2>Navbar</h2>
+    </div>
+  );
+}
